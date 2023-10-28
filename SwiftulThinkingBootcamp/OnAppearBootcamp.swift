8ab64c144fe509a3fd34e031b366f04ae509853c
OlegPlugaru <olegplugaru.md@gmail.com>
2023-10-28 17:30:34 +0300

    finish markups and documentation bootcamp

---
+++ SwiftulThinkingBootcamp/OnAppearBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  OnAppearBootcamp.swift
+//  SwiftulThinkingBootcamp
+//
+//  Created by Oleg Plugaru on 28.10.2023.
+//
+
+import SwiftUI
+
+struct OnAppearBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    OnAppearBootcamp()
+}
