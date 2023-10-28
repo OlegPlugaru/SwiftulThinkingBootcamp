88f120ca4ef4e526d705761863653841517b5983
OlegPlugaru <olegplugaru.md@gmail.com>
2023-10-28 19:46:33 +0300

    finished if else guard bootcamp

---
+++ SwiftulThinkingBootcamp/ifLetGuardBootcamp.swift
@@ -0,0 +1,73 @@
+//
+//  ifLetGuardBootcamp.swift
+//  SwiftulThinkingBootcamp
+//
+//  Created by Oleg Plugaru on 28.10.2023.
+//
+
+import SwiftUI
+
+struct ifLetGuardBootcamp: View {
+    
+    @State var currentUserID: String? = "test123"
+    @State var displayText: String? = nil
+    @State var isLoading: Bool = false
+    
+    var body: some View {
+        NavigationView {
+            VStack {
+                Text("Here we are practicing safe coding!")
+                
+                if let text = displayText {
+                    Text(text)
+                        .font(.title)
+                }
+                
+                // DO NOT USE ! EVER!!!
+                // DO NOT FORCE UNWRAP VALUES
+//                Text(displayText!)
+//                    .font(.title)
+                
+                if isLoading {
+                    ProgressView()
+                }
+                
+                Spacer()
+            }
+            .navigationTitle("Save Coding")
+            .onAppear {
+                loadData2()
+            }
+        }
+    }
+    
+    func loadData() {
+        if let userID = currentUserID {
+            isLoading = true
+            DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+                displayText = "This is the new data! User id is: \(userID)"
+                isLoading = false
+            }
+        } else {
+            displayText = "Error. There is no User ID!"
+        }
+    }
+    
+    func loadData2() {
+        guard let userID = currentUserID else {
+            displayText = "Error. There is no User ID!"
+            return
+        }
+        
+        isLoading = true
+        DispatchQueue.main.asyncAfter(deadline: .now() + 3) {
+            displayText = "This is the new data! User id is: \(userID)"
+            isLoading = false
+        }
+    }
+    
+}
+
+#Preview {
+    ifLetGuardBootcamp()
+}
