3bad083e5494cb26a955ad1d033c53113e378837
OlegPlugaru <olegplugaru.md@gmail.com>
2023-10-28 16:45:11 +0300

    finishing tabView and dark mode bootcamps

---
+++ SwiftulThinkingBootcamp/MarkupsAndDocumentationBootcamp.swift
@@ -0,0 +1,18 @@
+//
+//  MarkupsAndDocumentationBootcamp.swift
+//  SwiftulThinkingBootcamp
+//
+//  Created by Oleg Plugaru on 28.10.2023.
+//
+
+import SwiftUI
+
+struct MarkupsAndDocumentationBootcamp: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MarkupsAndDocumentationBootcamp()
+}
--- SwiftulThinkingBootcamp/TabViewBootcamp.swift
+++ SwiftulThinkingBootcamp/TabViewBootcamp.swift
@@ -8,11 +8,89 @@
 import SwiftUI
 
 struct TabViewBootcamp: View {
+    
+    @State var selectedTab: Int = 2
+    let icons: [String] = [
+        "heart.fill", "globe", "house.fill", "person.fill"
+    ]
+    
+//    init() {
+//        UITabBar.appearance().backgroundColor = .systemBackground
+//    }
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        
+        TabView {
+            ForEach(icons, id: \.self) { icon in
+                Image(systemName: icon)
+                    .resizable()
+                    .scaledToFit()
+                    .padding(30)
+            }
+        }
+        .background(
+            RadialGradient(gradient: Gradient(colors: [.red, .blue]), center: .center, startRadius: 5, endRadius: 300)
+        )
+        .frame(height: 300)
+        .tabViewStyle(.page)
+        
+        
+//        TabView(selection: $selectedTab) {
+//            HomeView(selectedTab: $selectedTab)
+//                .tabItem {
+//                    Image(systemName: "house.fill")
+//                    Text("Home")
+//                }
+//                .tag(0)
+//        
+//            Text("BROWSE TAB")
+//                .tabItem {
+//                    Image(systemName: "globe")
+//                    Text("Browse")
+//                }
+//                .tag(1)
+//            
+//            Text("PROFILE TAB")
+//                .tabItem {
+//                    Image(systemName: "person.fill")
+//                    Text("Profile")
+//                }
+//                .tag(2)
+//        }
+//        
+//        .accentColor(.red)
     }
 }
 
 #Preview {
     TabViewBootcamp()
+}
+
+struct HomeView: View {
+    
+    @Binding var selectedTab: Int
+    
+    var body: some View {
+        ZStack {
+            Color.red
+                .ignoresSafeArea()
+            
+            VStack {
+                Text("Home Tab")
+                    .font(.largeTitle)
+                    .foregroundColor(.white)
+                
+                Button {
+                    selectedTab = 2
+                } label: {
+                    Text("Go to profile")
+                        .font(.headline)
+                        .padding()
+                        .padding(.horizontal)
+                        .background(.white)
+                        .cornerRadius(10)
+                }
+            }
+        }
+    }
 }
